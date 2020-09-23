d602231e62a492200b5ac5545fdf4c78d516bb37
einarwar <einar.wigum.arbo@norconsult.com>
2020-09-23 11:13:13 +0200

    Added feature extraction to tools

--- setup.py
+++ setup.py
@@ -2,17 +2,18 @@ from distutils.core import setup
 setup(
   name = 'geotolktools',         # How you named your package folder (MyLib)
   packages = ['geotolktools'],   # Chose the same as "name"
-  version = '1.12',      # Start with a small number and increase it with every change you make
+  version = '1.201',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Parser for total sounding and related files',   # Give a short description about your library
   author = 'Einar Wigum Arbo',                   # Type in your name
   author_email = 'einararbo@gmail.com',      # Type in your E-Mail
   url = "https://github.com/einarwar/geotolk-tools",   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/Fundator/geotolk-tools/archive/v1.12.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/Fundator/geotolk-tools/archive/v.1.201.tar.gz',    # I explain this later on
   keywords = ['Geotechincs', 'Parsing'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'numpy',
           'pandas',
+          'scipy'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
